7b2054495690cd05a3fcfeac025128f1c073089a
woozway <22060468@zju.edu.cn>
2025-05-14 22:58:23 +0800

    -

---
+++ 算法基础课/第五讲 动态规划/线性DP/AcWing 897. 最长公共子序列.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+const int N = 1010;
+int n, m;
+char a[N], b[N];
+int f[N][N];
+
+//                     / 集合：所有由第1个序列的前i个字符，和第2个序列的前j个字符构成的LCS
+//      / 状态表示f[i,j]
+// DP问题               \ 属性：Max
+//      \
+//        状态计算：集合的划分按a[i],b[j]是否在答案中（00,01,10,11）
+// 其中00: f[i-1,j-1], 11: f[i-1,j-1] + 1, 01: f[i-1,j], 10: f[i,j-1]（并不完全等价但包含10，01同理）
+int main() {
+  cin >> n >> m;
+  cin >> a + 1 >> b + 1;
+
+  for (int i = 1; i <= n; i++)
+    for (int j = 1; j <= m; j++) {
+      f[i][j] = max(f[i-1][j], f[i][j-1]);
+      if (a[i] == b[j]) f[i][j] = max(f[i][j], f[i-1][j-1] + 1);
+    }
+  cout << f[n][m] << endl;
+
+  return 0;
+}
---
+++ 算法基础课/第五讲 动态规划/线性DP/AcWing 902. 最短编辑距离.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+const int N = 1010;
+int n, m;
+char a[N], b[N];
+int f[N][N];
+
+// 1. 状态表示：a. f[i,j]集合：字符串a[1~i]变成b[1~j]的最短编辑距离；b. 属性：Min
+// 2. 状态计算：集合划分（是否包含a[i], b[j]）
+//    00: f[i-1,j-1]+2, 01: f[i-1,j]+1, 
+//    10: f[i,j-1]+1, 11: f[i-1,j-1] if a[i]==b[j] else f[i-1,j-1]+1
+int main() {
+  scanf("%d%s", &n, a + 1);
+  scanf("%d%s", &m, b + 1);
+
+  for (int i = 0; i <= m; i++) f[0][i] = i;
+  for (int i = 0; i <= n; i++) f[i][0] = i;
+
+  for (int i = 1; i <= n; i++)
+    for (int j = 1; j <= m; j++) {
+      f[i][j] = min(f[i-1][j] + 1, f[i][j-1] + 1);
+      if (a[i] == b[j]) f[i][j] = min(f[i][j], f[i-1][j-1]);
+      else f[i][j] = min(f[i][j], f[i-1][j-1] + 1);
+    }
+
+  printf("%d\n", f[n][m]);
+
+  return 0;
+}
