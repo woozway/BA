ade901fc3e37289c1720d61585178a12f44531da
woozway <22060468@zju.edu.cn>
2025-05-15 22:05:58 +0800

    -

--- 算法基础课/第一讲 基础算法/高精度/AcWing 791. 高精度加法.cpp
+++ 算法基础课/第一讲 基础算法/高精度/AcWing 791. 高精度加法.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 using namespace std;
+const int base = 1e9; // 压9位
 
 vector<int> add(vector<int> &A, vector<int> &B) { // C=A+B, A>=0, B>=0
   if (A.size() < B.size()) return add(B, A);
@@ -10,8 +11,10 @@ vector<int> add(vector<int> &A, vector<int> &B) { // C=A+B, A>=0, B>=0
   for (int i = 0; i < A.size(); i++) {
     t += A[i]; // 进位
     if (i < B.size()) t += B[i];
-    C.push_back(t % 10);
-    t /= 10;
+    // C.push_back(t % 10);
+    // t /= 10;
+    C.push_back(t % base); // 压位优化
+    t /= base;
   }
   if (t) C.push_back(t);
 
@@ -23,12 +26,34 @@ int main() {
   cin >> a >> b;
 
   vector<int> A, B; // 由低到高倒着存，方便加减运算
-  for (int i = a.size()-1; i >= 0; i--) A.push_back(a[i] - '0');
-  for (int i = b.size()-1; i >= 0; i--) B.push_back(b[i] - '0');
+  // for (int i = a.size()-1; i >= 0; i--) A.push_back(a[i] - '0');
+  // for (int i = b.size()-1; i >= 0; i--) B.push_back(b[i] - '0');
+  for (int i=a.size()-1, s=0, j=0, t=1; i >= 0; i--) {
+    // A.push_back(a[i] - '0');
+    s += (a[i] - '0') * t;
+    j++, t *= 10;
+    if (j==9 || i==0) {
+      A.push_back(s);
+      s = j = 0;
+      t = 1;
+    }
+  }
+  for (int i=b.size()-1, s=0, j=0, t=1; i >= 0; i--) {
+    // B.push_back(b[i] - '0');
+    s += (b[i] - '0') * t;
+    j++, t *= 10;
+    if (j==9 || i==0) {
+      B.push_back(s);
+      s = j = 0;
+      t = 1;
+    }
+  }
 
   auto C = add(A, B);
 
-  for (int i = C.size()-1; i >= 0; i--) printf("%d", C[i]);
+  // for (int i = C.size()-1; i >= 0; i--) printf("%d", C[i]);
+  cout << C.back(); // 第一个数不需要最高位补零
+  for (int i = C.size()-2; i >= 0; i--) printf("%09d", C[i]);
   cout << endl;
 
   return 0;
---
+++ 算法基础课/第五讲 动态规划/计数类DP/AcWing 900. 整数划分.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+const int N = 1010, mod = 1e9 + 7;
+int n;
+int a[N];
+// int f[N][N];
+int f[N];
+
+//                       / 集合：从1~i中选，总体积恰好为j的方案数
+//       / 状态表示f[i,j]
+// DP问题                \ 属性：数量
+//       \ 
+//         状态计算：集合的划分（以最后一个物品（第i个）选择个数来划分0,1,2,..,s个）
+//          0: f[i-1,j], 1: f[i-1,j-i], 2: f[i-1,j-i*2], ..., s: f[i-1,j-i*s]
+
+// f[i,j] = f[i-1,j] + f[i-1,j-i] + f[i-1,j-i*2] + ... + f[i-1,j-i*s]
+// f[i,j-i] =          f[i-1,j-i] + f[i-1,j-i*2] + ... + f[i-1,j-i*s]
+// => f[i,j] = f[i-1,j] + f[i,j-i]
+// => f[j] = f[j] + f[j-i] (j从1~n, 滚动数组优化后)
+int main() {
+  cin >> n;
+
+  // f[0][0] = 1;
+  // for (int i = 1; i <= n; i++)
+  //   for (int j = 0; j <= n; j++) {
+  //     f[i][j] = f[i-1][j];
+  //     if (j >= i) f[i][j] = (f[i][j] + f[i][j-i]) % mod;
+  //   }
+  // cout << f[n][n] << endl;
+
+  // 滚动数组优化
+  f[0] = 1;
+  for (int i = 1; i <= n; i++)
+    for (int j = i; j <= n; j++)
+      f[j] = (f[j] + f[j-i]) % mod;
+
+  cout << f[n] << endl;
+  return 0;
+}
